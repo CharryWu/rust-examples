0b48dda68e228e37f22ab700601a1302f453367c
Charry Wu <wuzc.charry2013@gmail.com>
2025-03-09 13:59:34 -0700

    Add stack example

---
+++ stack.rs
@@ -0,0 +1,34 @@
+pub fn all() {
+    c();
+    d();
+    f();
+}
+
+fn a() {
+    println!("Calling A");
+    e();
+}
+
+fn b() {
+    println!("Calling B");
+}
+
+fn c() {
+    println!("Calling C");
+}
+
+fn d() {
+    println!("Calling D");
+    a();
+}
+
+fn e() {
+    println!("Calling E");
+}
+
+fn f() {
+    println!("Calling F");
+    b();
+}
+
+// xxd -g1 main
