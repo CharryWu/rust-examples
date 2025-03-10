5cbce9104f6139a7df4108c1c9b68300964a1032
Zichao Wu <zichao_wu@intuit.com>
2025-03-09 22:35:47 -0700

    Add move example

--- main.rs
+++ main.rs
@@ -1,6 +1,8 @@
 mod cat;
+mod move_example;
 mod stack;
 fn main() {
+    move_example::caller2();
     cat::kitten::meow();
     stack::all();
 }
---
+++ move_example.rs
@@ -0,0 +1,24 @@
+pub fn print_msg(msg: String) {
+    println!("{}", msg);
+    let msg2 = msg;
+    // println!("{}", msg); // msg becomes invalid after move, so this line throws error
+    println!("{}", msg2); // original `message` from caller1 is moved to `msg`, then moved to `msg2`
+} // `msg` is going out of scope, but nothing more will happen, because it was moved
+  // `msg2` is going out of scope, and its `drop` will be called which clears the underlying memory of string bytes from heap
+
+pub fn caller1() {
+    let message = String::from("test");
+    print_msg(message);
+    // println!("{}", message); // will throw error since `message` is moved into print_msg's argument `msg`
+} // `message` is going out of scope because it was moved into print_msg
+
+pub fn extend_msg(mut a: String) -> String {
+    a.push_str(" World");
+    a
+}
+
+pub fn caller2() {
+    let mut message = String::from("Hello");
+    message = extend_msg(message);
+    println!("{}", message);
+}
